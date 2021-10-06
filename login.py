9e419bd5dedcfeaf956b6184fa07fdbdf427e4a5
PyEddy <55319774+PyEddy@users.noreply.github.com>
2021-10-06 21:40:27 +0100

    Update login.py

--- login.py
+++ login.py
@@ -2,27 +2,31 @@ import configparser
 
 global user1, password1, user2, password2
 
+
+userID = 1
+
 config = configparser.RawConfigParser()
 config.read('login.txt')
-username = config.get('1', 'username')
-password = config.get('1', 'password')
-identity = config.get('1', "identity")
-OperatingSystem = config.get('1', 'OperatingSystem')
-Browser = config.get('1', 'Browser')
+username = config.get(str(userID), 'username')
+password = config.get(str(userID), 'password')
+identity = config.get(str(userID), "identity")
+OperatingSystem = config.get(str(userID), 'OperatingSystem')
+Browser = config.get(str(userID), 'Browser')
 
 class User:
     user1 = username
     password1 = password
     identity1 = identity
     browser1 = Browser
+    userID = userID + 1
     try:
         config = configparser.RawConfigParser()
         config.read('login.txt')
-        username = config.get('2', 'username')
-        password = config.get('2', 'password')
-        identity = config.get('2', "identity")
-        OperatingSystem = config.get('2', 'OperatingSystem')
-        Browser = config.get('2', 'Browser')
+        username = config.get(str(userID), 'username')
+        password = config.get(str(userID), 'password')
+        identity = config.get(str(userID), "identity")
+        OperatingSystem = config.get(str(userID), 'OperatingSystem')
+        Browser = config.get(str(userID), 'Browser')
         user2 = username
         password2 = password
         identity2 = identity
